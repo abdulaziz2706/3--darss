25c7b0907892df4856601b2484806c22e1099953
abdulaziz2706 <abdulaziz27707@gmail.com>
2025-02-14 15:40:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let brbalo = {
+
+}
+
+let ism = prompt(`Imingizni Kiriting`)
+
+let login = prompt(`Space loginingizni Kiriting`)
+
+let pass = prompt(`Space passwordingizni kiritng`)
+
+brbalo.Name = ism
+brbalo.login = login
+brbalo.password = pass
+console.log(brbalo);
+
+
+
+
+
+
+
